b6c4aac091c0f20d5768ff6352a18efa1d2deaa6
Bilal Iqbal <bilaliqbal14736@gmail.com>
2025-12-22 16:34:07 +0500

    Initial Commit

---
+++ HealthAdvisorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthAdvisorApp
+//
+//  Created by Bilal Iqbal on 22/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
